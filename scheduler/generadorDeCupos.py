21b3637db6ca1dd53e46ebfa29399c4adb07b318
fabricasw04 <fabricasw04@grupobaudelet.com>
2019-07-29 17:40:43 -0400

    arreglado lista de planificacion

--- scheduler/generadorDeCupos.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-from scheduler import procesoCupos
-
-def start():
-    scheduler = BackgroundScheduler()
-    #scheduler.add_job(procesoCupos.generarCantidadCupos, 'interval',seconds=10)
-    scheduler.add_job(procesoCupos.generarCantidadCupos, 'cron',hour=23,minute=55)
-    scheduler.start()
